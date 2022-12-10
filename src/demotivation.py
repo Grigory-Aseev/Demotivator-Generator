dab8fe143d3482442ddd1749dd6d5b3f04075ae0
grance <grinodin7@gmail.com>
2022-12-10 19:53:32 +0300

    add demotivator class, remove global variables, improve generation, remove saving image from demotivation

--- src/demotivation.py
+++ src/demotivation.py
@@ -2,15 +2,16 @@ from diffusers import StableDiffusionPipeline
 from transformers import FSMTForConditionalGeneration, FSMTTokenizer, VisionEncoderDecoderModel, ViTFeatureExtractor, \
     AutoTokenizer, pipeline
 import torch
-from simpledemotivators import Demotivator
+from demotivator import Demotivator
 import streamlit as st
-from PIL import Image
 
 ENG_TO_RU_MODEL = "facebook/wmt19-en-ru"
+RU_TO_ENG_MODEL = "facebook/wmt19-ru-en"
 IMAGE_MODEL = "DGSpitzer/Cyberpunk-Anime-Diffusion"
 IMAGE_CAPTION_MODEL = "nlpconnect/vit-gpt2-image-captioning"
 DETECTION_LANGUAGE_MODEL = "papluca/xlm-roberta-base-language-detection"
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+GEN_KWARGS = {"max_length": 80, "num_beams": 5}
 
 
 @st.cache(allow_output_mutation=True, suppress_st_warning=True)
@@ -22,29 +23,30 @@ def get_pipe_image():
         return StableDiffusionPipeline.from_pretrained(IMAGE_MODEL, requires_safety_checker=False).to(DEVICE)
 
 
-def generate_image(text, path, steps, height, width):
+def generate_image(text, steps, height, width):
     pipe_image = get_pipe_image()
     pipe_image.safety_checker = lambda images, clip_input: (images, False)
     image = pipe_image(text, num_inference_steps=steps, height=height, width=width).images[0]
-    image.save(path)
+    return image
 
 
 @st.cache(allow_output_mutation=True, suppress_st_warning=True)
-def get_pipe_translate():
-    return FSMTTokenizer.from_pretrained(ENG_TO_RU_MODEL), FSMTForConditionalGeneration.from_pretrained(ENG_TO_RU_MODEL)
+def get_pipe_translate(lang):
+    model = RU_TO_ENG_MODEL if lang == "ru" else ENG_TO_RU_MODEL
+    return FSMTTokenizer.from_pretrained(model), FSMTForConditionalGeneration.from_pretrained(model)
 
 
-def translate(text):
-    tokenizer, model = get_pipe_translate()
+def translate(text, lang):
+    tokenizer, model = get_pipe_translate(lang)
     input_ids = tokenizer.encode(text, return_tensors="pt")
-    outputs = model.generate(input_ids)
+    outputs = model.generate(input_ids, **GEN_KWARGS)
     decoded = tokenizer.decode(outputs[0], skip_special_tokens=True)
     return decoded
 
 
-def demotivate(image_dir, watermark="grance", top_text="", bottom_text=""):
+def demotivate(image, watermark="grance", top_text="", bottom_text=""):
     dem = Demotivator(top_text, bottom_text)
-    dem.create(image_dir, result_filename=image_dir, watermark=watermark)
+    return dem.create(image, watermark=watermark)
 
 
 @st.cache(allow_output_mutation=True, suppress_st_warning=True)
@@ -53,13 +55,12 @@ def get_pipe_img_caption():
         IMAGE_CAPTION_MODEL), VisionEncoderDecoderModel.from_pretrained(IMAGE_CAPTION_MODEL).to(DEVICE)
 
 
-def get_image_caption(image_paths):
+def get_image_caption(img):
     tokenizer, feature_extractor, model = get_pipe_img_caption()
-    gen_kwargs = {"max_length": 80, "num_beams": 8}
-    images = [Image.open(image_paths)]
+    images = [img]
     pixel_values = feature_extractor(images=images, return_tensors="pt").pixel_values
     pixel_values = pixel_values.to(DEVICE)
-    output_ids = model.generate(pixel_values, **gen_kwargs)
+    output_ids = model.generate(pixel_values, **GEN_KWARGS)
     preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
     preds = [pred.strip() for pred in preds]
     return preds[0]
@@ -80,15 +81,21 @@ def detect_language(text):
         return None
 
 
-def generate_demotivator(text, steps, height, width, path):
-    top_text = text[:30 if len(text) >= 30 else len(text)]
+def generate_demotivator(text, steps, height, width):
+    top_text = text
     lang_prob = detect_language(text)
     if lang_prob is not None:
         language = max(lang_prob, key=lang_prob.get)
     else:
         language = "undefined"
-    generate_image(text, path, steps, height, width)
-    text = get_image_caption(path)
+
+    """it is better to translate the text into English
+    because StableDiffusion produces more suitable results from the English text"""
+
     if language == "ru":
-        text = translate(text)
-    demotivate(image_dir=path, top_text=top_text, bottom_text=text)
+        text = translate(text, language)
+    img = generate_image(text, steps, height, width)
+    text = get_image_caption(img)
+    if language == "ru":
+        text = translate(text, "en")
+    return demotivate(image=img, top_text=top_text, bottom_text=text)
--- src/main.py
+++ src/main.py
@@ -1,45 +1,48 @@
 import streamlit as st
 from demotivation import generate_demotivator
-from os.path import exists
+from PIL import Image
+from io import BytesIO
 
-RESULT_DIR = "..\\images\\result.jpg"
+IMAGE_EXAMPLE_DIR = "..\\images\\result.jpg"
 
 
-def update_image_section():
-    if exists(RESULT_DIR):
-        image.empty()
-        image.image(RESULT_DIR)
-        with open(RESULT_DIR, "rb") as file:
-            button.download_button(label="Download", data=file, file_name=RESULT_DIR)
+def update_image_section(img):
+    st.image(img)
+    buf = BytesIO()
+    img.save(buf, format="JPEG")
+    byte_im = buf.getvalue()
+    st.download_button(label="Download", data=byte_im, file_name=IMAGE_EXAMPLE_DIR)
 
 
 def init_image_section():
-    global image, button
-    image = st.empty()
-    button = st.empty()
+    img = Image.open(IMAGE_EXAMPLE_DIR)
+    st.session_state.image = img
 
 
 def main():
     st.title("Generator demotivators with Streamlit by [grance](https://github.com/Grigory-Aseev)")
     st.sidebar.header("Demotivation")
     st.sidebar.markdown("Generate your own demotivator based on your text!")
-    text = st.sidebar.text_input("Enter your sentence", "One-eyed ghoul kaneki-ken")
+    text = st.sidebar.text_input("Enter your sentence", "One-eyed ghoul kaneki-ken", max_chars=50)
     steps = st.sidebar.slider("Numbers of inference steps", value=50, min_value=4, max_value=100,
                               help="More steps usually lead to a higher quality of image and slower inference.")
     height = st.sidebar.slider("Height", value=512, min_value=128, max_value=512,
                                help="After receiving the image, the result of the neural network model will be resized to the specified height.")
     width = st.sidebar.slider("Width", value=512, min_value=128, max_value=512,
                               help="After receiving the image, the result of the neural network model will be resized to the specified width.")
+
+    if "image" not in st.session_state:
+        init_image_section()
+
     get_button = st.sidebar.button("Generate image")
-    init_image_section()
-    update_image_section()
     try:
         if get_button:
-            generate_demotivator(text, steps, height, width, RESULT_DIR)
-            update_image_section()
+            st.session_state.image = generate_demotivator(text, steps, height, width)
             st.sidebar.success("Success!")
     except Exception as e:
         st.sidebar.error("Failed with error: {}".format(e))
+
+    update_image_section(st.session_state.image)
 
 
 if __name__ == "__main__":
